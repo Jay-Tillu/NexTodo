23ebaaa46c5d5e4b8e051c29176e2b4d6bf1e9f2
Jay Tillu <26956655+Jay-Tillu@users.noreply.github.com>
2024-12-12 15:52:11 +0530

    Project Cleaned

--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const HomeComponent = () => {
+const index = () => {
   return (
     <View>
-      <Text>HomeComponent</Text>
+      <Text>index</Text>
     </View>
   )
 }
 
-export default HomeComponent
+export default index
